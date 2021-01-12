369cb23d3afd883d5ad96719ea4fcb57f27f6ee1
sahil-au7 <sahilyadav601@gmail.com>
2021-01-12 23:26:12 +0530

    fetching data from the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import { FormControl, MenuItem, Select } from "@material-ui/core";
 import "./App.css";
-
+import React, { useState, useEffect } from "react";
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const fetchingData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+          console.log(countries);
+        });
+    };
+    fetchingData();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>covid</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* Header */}
+      {/* Title +drop down feild */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
---
+++ src/utilities/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4HCd3_lCm4UepTb72pnvEGQ37co4zYvQ",
+  authDomain: "covid-19-tracker-a4103.firebaseapp.com",
+  projectId: "covid-19-tracker-a4103",
+  storageBucket: "covid-19-tracker-a4103.appspot.com",
+  messagingSenderId: "193504775778",
+  appId: "1:193504775778:web:9372fe88b1f00f5f644b3b",
+  measurementId: "G-DXH5TNV7EM",
+};
+
+export default firebaseConfig;
